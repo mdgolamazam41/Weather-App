99343e927cdd34d1e54a15f8c09589502a104dfb
Md. Golam Azam <mg.azam41@gmail.com>
2016-12-23 19:01:17 +0600

    Update CurrentWeather.swift

--- rainyshinycloudy/CurrentWeather.swift
+++ rainyshinycloudy/CurrentWeather.swift
@@ -1,10 +1,3 @@
-//
-//  CurrentWeather.swift
-//  rainyshinycloudy
-//
-//  Created by Caleb Stultz on 7/27/16.
-//  Copyright © 2016 Caleb Stultz. All rights reserved.
-//
 
 import UIKit
 import Alamofire
